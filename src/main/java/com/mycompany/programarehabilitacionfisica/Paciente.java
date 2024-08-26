e593f4e77664c56831e0b9d1dd3fdc2609193452
Ian Guerrero <ian.guerrero.v@mail.pucv.cl>
2024-08-26 14:51:01 -0400

    Carga Inicial

---
+++ src/main/java/com/mycompany/programarehabilitacionfisica/Paciente.java
@@ -0,0 +1,41 @@
+package com.mycompany.programarehabilitacionfisica;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author iangu
+ */
+public class Paciente {
+    private String nombre;
+    private int edad;
+    private String ocupacion;
+    
+    
+
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public int getEdad() {
+        return edad;
+    }
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    public String getOcupacion() {
+        return ocupacion;
+    }
+    public void setOcupacion(String ocupacion) {
+        this.ocupacion = ocupacion;
+    }
+    
+    
+    
+    
+}
---
+++ src/main/java/com/mycompany/programarehabilitacionfisica/ProgramaRehabilitacionFisica.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.programarehabilitacionfisica;
+
+/**
+ *
+ * @author iangu
+ */
+public class ProgramaRehabilitacionFisica {
+
+    public static void main(String[] args) {
+        
+        
+        System.out.println("Hello World!");
+    }
+}
