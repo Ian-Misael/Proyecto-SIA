cee6df7b02e378941c2affd25618e96f92addbf0
Felipe <>
2024-08-26 21:07:49 -0400

    Utilidades

---
+++ src/main/java/com/mycompany/programarehabilitacionfisica/Utilidades.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.programarehabilitacionfisica;
+
+/**
+ *
+ * @author felip
+ */
+public class Utilidades {
+    
+    public static void limpiarPantalla()
+    {
+        for (int i = 0; i < 50; i++)
+        {
+            System.out.println();
+        }
+    }
+}
