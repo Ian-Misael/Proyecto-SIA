8fd6a09663a87738468b610606a58892ad657541
Felipe <>
2024-08-26 20:53:22 -0400

    Menu y clases SesionTerapia, Paciente, Utilidades

--- src/main/java/com/mycompany/programarehabilitacionfisica/ProgramaRehabilitacionFisica.java
+++ src/main/java/com/mycompany/programarehabilitacionfisica/ProgramaRehabilitacionFisica.java
@@ -3,6 +3,10 @@
  */
 
 package com.mycompany.programarehabilitacionfisica;
+import java.util.Scanner;
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  *
@@ -11,12 +15,50 @@ package com.mycompany.programarehabilitacionfisica;
 public class ProgramaRehabilitacionFisica {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int opcion;
         
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
-        System.out.println("Hello World!");
+        
+        do{
+            Utilidades.limpiarPantalla();
+            System.out.println("--- Menú de Gestión de Rehabilitación Física ---");
+            System.out.println("1. Registrar un nuevo paciente");
+            System.out.println("2. Registrar nueva sesion de terapia");
+            System.out.println("3. Seguimiento paciente");
+            System.out.println("4. Reporte Progreso");
+            System.out.println("5. Salir");
+            
+            opcion = scanner.nextInt();
+            
+            switch(opcion)
+            {
+                case 1:
+                    System.out.println("Case 1");
+                    //opcion = scanner.nextInt();
+                    //while (opcion != 6)
+                    //{
+                     //   opcion = scanner.nextInt();
+                    //}
+                    break;
+                case 2:
+                    System.out.println("Case 2");
+                    break;
+                case 3:
+                    System.out.println("Case 3");
+                    break;
+                case 4: 
+                    System.out.println("Case 4");
+                    break;
+                default:
+                    if (opcion == 5)
+                    {
+                        System.out.println("Saliendo del programa....");
+                        break;
+                    }
+                    System.out.println("Opcion no valida!!");
+                    break;
+            }
+        } while(opcion != 5);
+        
     }
 }
