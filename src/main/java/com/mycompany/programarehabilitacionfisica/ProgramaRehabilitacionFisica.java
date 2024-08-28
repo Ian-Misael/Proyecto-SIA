9c2aaaf67c6399b9e3429651a285a8d7c8fff3ca
jgera <jgera@10.8.233.44>
2024-08-28 15:25:29 -0400

    case 2 funciona case 3 algo

--- src/main/java/com/mycompany/programarehabilitacionfisica/ProgramaRehabilitacionFisica.java
+++ src/main/java/com/mycompany/programarehabilitacionfisica/ProgramaRehabilitacionFisica.java
@@ -4,10 +4,29 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
+import java.time.LocalDate;
 
 public class ProgramaRehabilitacionFisica {
-
+    
     public static void main(String[] args) throws IOException {
+        
+        HashMap<String, Paciente> tablaHashPacientes = new HashMap<>();
+
+        Paciente paciente1 = new Paciente("Juan","20.180.417-5", 25, "Viña del Mar", "Resfriado");
+        Paciente paciente2 = new Paciente("Ian","21.584.598-2", 20, "Quilpue", "Muerto");
+        Paciente paciente3 = new Paciente("Felipe","21.734.001-2", 20, "Quilpue", "Hemorroides");
+        
+        paciente1.agregarSesion(LocalDate.now());
+        paciente1.agregarSesion(LocalDate.now());
+        paciente1.agregarSesion(LocalDate.now());
+        paciente1.agregarSesion(LocalDate.now());
+        
+        
+        //paciente2.agregarSesion("28/08/2024");
+        //paciente3.agregarSesion("28/08/2024");
+        tablaHashPacientes.put(paciente1.getRut(),paciente1);
+        tablaHashPacientes.put(paciente2.getRut(),paciente2);
+        tablaHashPacientes.put(paciente3.getRut(),paciente3);
         
         //Scanner scanner = new Scanner(System.in);
         BufferedReader lector = new BufferedReader (new InputStreamReader(System.in));
@@ -19,7 +38,7 @@ public class ProgramaRehabilitacionFisica {
             
             opcion = Integer.parseInt(lector.readLine());
             
-            opcionMenu(opcion);
+            opcionMenu(opcion,tablaHashPacientes);
             
             Utilidades.presioneTeclaParaContinuar();
             
@@ -30,29 +49,39 @@ public class ProgramaRehabilitacionFisica {
         
         Utilidades.limpiarPantalla();
         
-        System.out.println("--- Menú de Gestion de Rehabilitacion Fisica ---");
+        System.out.println("--- Menu de Gestion de Rehabilitacion Fisica ---");
         System.out.println("1. Registrar un nuevo paciente");
-        System.out.println("2. Registrar nueva sesion de terapia");
-        System.out.println("3. Seguimiento paciente");
+        System.out.println("2. Seguimiento paciente");
+        System.out.println("3. Registrar nueva sesion de terapia");
         System.out.println("4. Reporte Progreso");
         System.out.println("5. Salir");
     }
     
-    public static void opcionMenu(int opcion) {
-        
-        HashMap<String, Paciente> tablaHashPacientes = new HashMap<>();
-        
+    public static void opcionMenu(int opcion, HashMap<String, Paciente> tablaHashPacientes) throws IOException {
+        BufferedReader lector = new BufferedReader (new InputStreamReader(System.in));
         switch(opcion) {
             case 1:
                 Paciente nuevoPaciente = new Paciente();
-                
-                nuevoPaciente.agregarPaciente(tablaHashPacientes);
+                nuevoPaciente.poblar();
+                tablaHashPacientes.put(nuevoPaciente.getRut(), nuevoPaciente);
                 break;
             case 2:
-                System.out.println("Case 2");
+                System.out.print("Ingrese el rut del paciente: ");
+                String rut = lector.readLine();
+                rut = Utilidades.formatearRut(rut);
+                
+                Paciente pacienteBuscado = tablaHashPacientes.get(rut);
+                pacienteBuscado.mostrarDatos();
+                
                 break;
             case 3:
-                System.out.println("Case 3");
+                System.out.println("Ingrese el rut del paciente: ");
+                String rut2 = lector.readLine();
+                Paciente pacienteActual = tablaHashPacientes.get(rut2);
+                System.out.println("Ingrese la fecha de la sesión(DD/MM/YY):");
+                String input = lector.readLine();
+                LocalDate fecha = LocalDate.parse(input);
+                pacienteActual.agregarSesion(fecha);
                 break;
             case 4: 
                 System.out.println("Case 4");
