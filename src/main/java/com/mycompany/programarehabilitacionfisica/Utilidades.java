199323c8b876768a2dd446734fcefdd025293898
Ian Guerrero <ian.guerrero.v@mail.pucv.cl>
2024-08-28 10:12:43 -0400

    Modularización

--- src/main/java/com/mycompany/programarehabilitacionfisica/Utilidades.java
+++ src/main/java/com/mycompany/programarehabilitacionfisica/Utilidades.java
@@ -1,20 +1,12 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.programarehabilitacionfisica;
+
 import java.util.Scanner;
 
-/**
- *
- * @author felip
- */
 public class Utilidades {
     
-    public static void limpiarPantalla()
-    {
-        for (int i = 0; i < 50; i++)
-        {
+    // Métodos de la Clase
+    public static void limpiarPantalla() {
+        for (int i = 0; i < 50; i++) {
             System.out.println();
         }
     }
@@ -27,32 +19,25 @@ public class Utilidades {
         int largo = rut.length();
         //System.out.println(largo);
         int contador = 0;
-        for (int i = largo - 1; i >= 0; i--)
-        {
-            if (i == (largo - 1))
-            {
+        for (int i = largo - 1; i >= 0; i--) {
+            if (i == (largo - 1)) {
                 rutFormateado = rutFormateado + rut.charAt(i);
                 rutFormateado = "-" + rutFormateado;  
             }
-            else
-            {
+            else {
                 rutFormateado =  rut.charAt(i) + rutFormateado;
                 contador++;
             
-                if (contador == 3 && i != 0)
-                {
+                if (contador == 3 && i != 0) {
                     rutFormateado = "." + rutFormateado;
                     contador = 0;
                 }
-                
             }
         }
         return rutFormateado;
-        
     }
     
-    public static void presioneTeclaParaContinuar()
-    {
+    public static void presioneTeclaParaContinuar() {
         Scanner scanner = new Scanner(System.in);
         String enter;
         System.out.println("Presione enter para continuar..");
