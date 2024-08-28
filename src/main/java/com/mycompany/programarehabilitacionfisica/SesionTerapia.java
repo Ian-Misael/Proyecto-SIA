3e25bb47bb83f754b0fb39e436f058e59d5f52ab
Ian Guerrero <ian.guerrero.v@mail.pucv.cl>
2024-08-28 10:12:37 -0400

    Modularización

--- src/main/java/com/mycompany/programarehabilitacionfisica/SesionTerapia.java
+++ src/main/java/com/mycompany/programarehabilitacionfisica/SesionTerapia.java
@@ -1,20 +1,14 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.programarehabilitacionfisica;
 
-/**
- *
- * @author felip
- */
 public class SesionTerapia {
+    
     private String fecha;
     private String tipoTerapia;
     private String duracion;
     private String observaciones;
     private int calificacionMejora;
 
+    // Métodos Constructores
     public SesionTerapia(String fecha, String tipoTerapia, String duracion, String observaciones, int calificacionMejora) {
         this.fecha = fecha;
         this.tipoTerapia = tipoTerapia;
@@ -23,47 +17,47 @@ public class SesionTerapia {
         this.calificacionMejora = calificacionMejora;
     }
 
+    // Métodos Getter y Setter
     public String getFecha() {
         return fecha;
+    }
+    
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
     }
 
     public String getTipoTerapia() {
         return tipoTerapia;
     }
+    
+    public void setTipoTerapia(String tipoTerapia) {
+        this.tipoTerapia = tipoTerapia;
+    }
 
     public String getDuracion() {
         return duracion;
+    }
+    
+    public void setDuracion(String duracion) {
+        this.duracion = duracion;
     }
 
     public String getObservaciones() {
         return observaciones;
     }
 
-    public int getCalificacionMejora() {
-        return calificacionMejora;
-    }
-
-    public void setFecha(String fecha) {
-        this.fecha = fecha;
-    }
-
-    public void setTipoTerapia(String tipoTerapia) {
-        this.tipoTerapia = tipoTerapia;
-    }
-
-    public void setDuracion(String duracion) {
-        this.duracion = duracion;
-    }
-
     public void setObservaciones(String observaciones) {
         this.observaciones = observaciones;
+    }
+    
+    public int getCalificacionMejora() {
+        return calificacionMejora;
     }
 
     public void setCalificacionMejora(int calificacionMejora) {
         this.calificacionMejora = calificacionMejora;
     }
     
-    
-    
+    // Métodos de la Clase
     
 }
