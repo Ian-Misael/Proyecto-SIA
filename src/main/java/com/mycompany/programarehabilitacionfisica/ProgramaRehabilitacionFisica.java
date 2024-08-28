00367a90983460022c8fd96b20e055eb66ee5892
Ian Guerrero <ian.guerrero.v@mail.pucv.cl>
2024-08-28 10:12:07 -0400

    Modularización

--- src/main/java/com/mycompany/programarehabilitacionfisica/ProgramaRehabilitacionFisica.java
+++ src/main/java/com/mycompany/programarehabilitacionfisica/ProgramaRehabilitacionFisica.java
@@ -1,62 +1,69 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.mycompany.programarehabilitacionfisica;
-import java.util.Scanner;
-import java.util.HashMap;
-import java.util.ArrayList;
-import java.util.List;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
 
-/**
- *
- * @author iangu
- */
 public class ProgramaRehabilitacionFisica {
 
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        int opcion;
-        HashMap<String, Paciente> tablaHashPacientes = new HashMap<>();
+    public static void main(String[] args) throws IOException {
         
+        //Scanner scanner = new Scanner(System.in);
+        BufferedReader lector = new BufferedReader (new InputStreamReader(System.in));
+        
+        int opcion;
         
         do{
-            Utilidades.limpiarPantalla();
-            System.out.println("--- Menú de Gestión de Rehabilitación Física ---");
-            System.out.println("1. Registrar un nuevo paciente");
-            System.out.println("2. Registrar nueva sesion de terapia");
-            System.out.println("3. Seguimiento paciente");
-            System.out.println("4. Reporte Progreso");
-            System.out.println("5. Salir");
+            menu();
             
-            opcion = scanner.nextInt();
-             
-            switch(opcion)
-            {
-                case 1:
-                    Paciente nuevoPaciente = new Paciente();
-                    nuevoPaciente.agregarPaciente(scanner,tablaHashPacientes);
-                    break;
-                case 2:
-                    System.out.println("Case 2");
-                    break;
-                case 3:
-                    System.out.println("Case 3");
-                    break;
-                case 4: 
-                    System.out.println("Case 4");
-                    break;
-                default:
-                    if (opcion == 5)
-                    {
-                        System.out.println("Saliendo del programa....");
-                        break;
-                    }
-                    System.out.println("Opcion no valida!!");
-                    break;
-            }
+            opcion = Integer.parseInt(lector.readLine());
+            
+            opcionMenu(opcion);
+            
             Utilidades.presioneTeclaParaContinuar();
+            
         } while(opcion != 5);
+    }
+    
+    public static void menu() {
         
+        Utilidades.limpiarPantalla();
+        
+        System.out.println("--- Menú de Gestion de Rehabilitacion Fisica ---");
+        System.out.println("1. Registrar un nuevo paciente");
+        System.out.println("2. Registrar nueva sesion de terapia");
+        System.out.println("3. Seguimiento paciente");
+        System.out.println("4. Reporte Progreso");
+        System.out.println("5. Salir");
+    }
+    
+    public static void opcionMenu(int opcion) {
+        
+        HashMap<String, Paciente> tablaHashPacientes = new HashMap<>();
+        
+        switch(opcion) {
+            case 1:
+                Paciente nuevoPaciente = new Paciente();
+                
+                nuevoPaciente.agregarPaciente(tablaHashPacientes);
+                break;
+            case 2:
+                System.out.println("Case 2");
+                break;
+            case 3:
+                System.out.println("Case 3");
+                break;
+            case 4: 
+                System.out.println("Case 4");
+                break;
+            default:
+                if (opcion == 5) {
+                    System.out.println("Saliendo del programa....");
+                    break;
+                }
+                System.out.println("Opcion no valida!!");
+                break;
+        }
     }
 }
