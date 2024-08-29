70e11ed9ee1bbfaf7cc61f249b34b37705d88238
Ian Guerrero <ian.guerrero.v@mail.pucv.cl>
2024-08-28 23:03:19 -0400

    Interación por consola

--- src/main/java/com/mycompany/programarehabilitacionfisica/ProgramaRehabilitacionFisica.java
+++ src/main/java/com/mycompany/programarehabilitacionfisica/ProgramaRehabilitacionFisica.java
@@ -3,96 +3,176 @@ package com.mycompany.programarehabilitacionfisica;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.HashMap;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class ProgramaRehabilitacionFisica {
     
     public static void main(String[] args) throws IOException {
         
         HashMap<String, Paciente> tablaHashPacientes = new HashMap<>();
-
-        Paciente paciente1 = new Paciente("Juan","20.180.417-5", 25, "Viña del Mar", "Resfriado");
-        Paciente paciente2 = new Paciente("Ian","21.584.598-2", 20, "Quilpue", "Muerto");
-        Paciente paciente3 = new Paciente("Felipe","21.734.001-2", 20, "Quilpue", "Hemorroides");
-        
-        paciente1.agregarSesion(LocalDate.now());
-        paciente1.agregarSesion(LocalDate.now());
-        paciente1.agregarSesion(LocalDate.now());
-        paciente1.agregarSesion(LocalDate.now());
-        
-        
-        //paciente2.agregarSesion("28/08/2024");
-        //paciente3.agregarSesion("28/08/2024");
-        tablaHashPacientes.put(paciente1.getRut(),paciente1);
-        tablaHashPacientes.put(paciente2.getRut(),paciente2);
-        tablaHashPacientes.put(paciente3.getRut(),paciente3);
-        
-        //Scanner scanner = new Scanner(System.in);
         BufferedReader lector = new BufferedReader (new InputStreamReader(System.in));
+        
+        // Se ingresan ejemplos
+        ingresoCasos(tablaHashPacientes);
         
         int opcion;
         
-        do{
+        // Operación Principal del Menú
+        do {
             menu();
-            
             opcion = Integer.parseInt(lector.readLine());
-            
-            opcionMenu(opcion,tablaHashPacientes);
-            
+            opcionMenu(opcion, tablaHashPacientes);
             Utilidades.presioneTeclaParaContinuar();
             
         } while(opcion != 5);
     }
     
     public static void menu() {
-        
         Utilidades.limpiarPantalla();
-        
-        System.out.println("--- Menu de Gestion de Rehabilitacion Fisica ---");
-        System.out.println("1. Registrar un nuevo paciente");
-        System.out.println("2. Seguimiento paciente");
-        System.out.println("3. Registrar nueva sesion de terapia");
-        System.out.println("4. Reporte Progreso");
-        System.out.println("5. Salir");
+        System.out.println("--- Menu de Gestion de Rehabilitacion Fisica ---\n");
+        System.out.println(" 1. Registrar un nuevo paciente");
+        System.out.println(" 2. Informacion de pacientes");
+        System.out.println(" 3. Registrar nueva sesion de terapia");
+        System.out.println(" 4. Reporte Progreso");
+        System.out.println(" 5. Salir");
     }
     
-    public static void opcionMenu(int opcion, HashMap<String, Paciente> tablaHashPacientes) throws IOException {
-        BufferedReader lector = new BufferedReader (new InputStreamReader(System.in));
-        switch(opcion) {
+public static void opcionMenu(int opcion, HashMap<String, Paciente> tablaHashPacientes) throws IOException {
+    BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
+    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    switch (opcion) {
+        case 1:
+            Utilidades.limpiarPantalla();
+            System.out.println("--- Registro de Paciente ---\n");
+            Paciente nuevoPaciente = new Paciente();
+            nuevoPaciente.poblar();
+            tablaHashPacientes.put(nuevoPaciente.getRut(), nuevoPaciente);
+            break;
+        case 2:
+            Utilidades.limpiarPantalla();
+            menu2();
+            int opcion2 = Integer.parseInt(lector.readLine());
+            opcionMenu2(opcion2, tablaHashPacientes);
+            break;
+        case 3:
+            Utilidades.limpiarPantalla();
+            System.out.print("Ingrese el rut del paciente: ");
+            String rutAux = lector.readLine();
+            rutAux = Utilidades.formatearRut(rutAux);
+            Paciente pacienteActual = tablaHashPacientes.get(rutAux);
+
+            if (pacienteActual != null) {
+                Utilidades.limpiarPantalla();
+                System.out.print("Ingrese la fecha de la sesion (dd/MM/yyyy): ");
+                String inputFecha = lector.readLine();
+                try {
+                    LocalDate fecha = LocalDate.parse(inputFecha, formatter);
+
+                    // Solicitar más detalles sobre la sesión
+                    System.out.print("Ingrese el tipo de terapia: ");
+                    String tipoTerapia = lector.readLine();
+
+                    System.out.print("Ingrese la duracion de la terapia: ");
+                    String duracion = lector.readLine();
+
+                    System.out.print("Ingrese observaciones: ");
+                    String observaciones = lector.readLine();
+
+                    System.out.print("Ingrese la calificacion de mejora (1-10): ");
+                    int calificacionMejora = Integer.parseInt(lector.readLine());
+
+                    // Crear una nueva sesión de terapia con los detalles ingresados
+                    SesionTerapia nuevaSesion = new SesionTerapia(fecha, tipoTerapia, duracion, observaciones, calificacionMejora);
+                    pacienteActual.agregarSesion(nuevaSesion);
+
+                    System.out.println("\nSesion registrada con exito.\n");
+                } catch (DateTimeParseException e) {
+                    System.out.println("\nFecha ingresada no valida. Asegurese de usar el formato dd/MM/yyyy.\n");
+                }
+            } else {
+                System.out.println("\nPaciente no encontrado.\n");
+            }
+            break;
+        case 4:
+            Utilidades.limpiarPantalla();
+            System.out.println("Funcionalidad de Reporte Progreso en desarrollo.");
+            break;
+        default:
+            if (opcion == 5) {
+                System.out.println("Saliendo del programa....");
+            } else {
+                System.out.println("Opcion no valida!!");
+            }
+            break;
+        }
+    }
+
+    public static void menu2() {
+        Utilidades.limpiarPantalla();
+        System.out.println("--- Seleccione visualizacion de Pacientes ---\n");
+        System.out.println(" 1. Listado de pacientes");
+        System.out.println(" 2. Seguimiento de paciente");
+    }
+    
+    public static void opcionMenu2(int opcion, HashMap<String, Paciente> tablaHashPacientes) throws IOException {
+        BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
+        
+        switch (opcion) {
             case 1:
-                Paciente nuevoPaciente = new Paciente();
-                nuevoPaciente.poblar();
-                tablaHashPacientes.put(nuevoPaciente.getRut(), nuevoPaciente);
+                Utilidades.limpiarPantalla();
+                System.out.println("--- Listado de pacientes registrados ---\n");
+                System.out.println("---------------------------------------");
+                
+                for (Paciente paciente : tablaHashPacientes.values()) {
+                System.out.println(" Nombre: " + paciente.getNombre());
+                System.out.println(" RUT: " + paciente.getRut());
+                System.out.println(" Edad: " + paciente.getEdad());
+                System.out.println("---------------------------------------");
+                }
+                System.out.println("");
                 break;
-            case 2:
+            case 2:    
+                Utilidades.limpiarPantalla();
                 System.out.print("Ingrese el rut del paciente: ");
                 String rut = lector.readLine();
                 rut = Utilidades.formatearRut(rut);
-                
+
                 Paciente pacienteBuscado = tablaHashPacientes.get(rut);
-                pacienteBuscado.mostrarDatos();
-                
-                break;
-            case 3:
-                System.out.println("Ingrese el rut del paciente: ");
-                String rut2 = lector.readLine();
-                Paciente pacienteActual = tablaHashPacientes.get(rut2);
-                System.out.println("Ingrese la fecha de la sesión(DD/MM/YY):");
-                String input = lector.readLine();
-                LocalDate fecha = LocalDate.parse(input);
-                pacienteActual.agregarSesion(fecha);
-                break;
-            case 4: 
-                System.out.println("Case 4");
+                if (pacienteBuscado != null) {
+                    pacienteBuscado.mostrarDatos();
+                } else {
+                    System.out.println("\nPaciente no encontrado.\n");
+                }
                 break;
             default:
-                if (opcion == 5) {
-                    System.out.println("Saliendo del programa....");
-                    break;
-                }
                 System.out.println("Opcion no valida!!");
                 break;
         }
     }
+    
+    public static void ingresoCasos(HashMap<String, Paciente> tablaHashPacientes) {
+        Paciente paciente1 = new Paciente("Juan","20.180.417-5", 25, "Viña del Mar", "Resfriado");
+        Paciente paciente2 = new Paciente("Felipe","21.734.001-2", 20, "Quilpue", "Hemorroides");
+        Paciente paciente3 = new Paciente("Ian","21.584.598-2", 20, "Quilpue", "Muerto");
+        
+        paciente1.agregarSesion(LocalDate.now());
+        paciente2.agregarSesion(LocalDate.of(2004, 12, 20));
+        paciente3.agregarSesion(LocalDate.of(2022, 4, 10));
+        
+        SesionTerapia sesion11 = new SesionTerapia(LocalDate.of(2074, 3, 8), "Conductual", "4 Horas", "Mantiene las apuestas en casinos", 2);
+        paciente1.agregarSesion(sesion11);
+        SesionTerapia sesion21 = new SesionTerapia(LocalDate.of(2019, 10, 10), "Logoterapia", "30 Minutos", "Aun no encuentra el sentido de la Vida", 5);
+        paciente2.agregarSesion(sesion21);
+        SesionTerapia sesion31 = new SesionTerapia(LocalDate.of(2004, 5, 25), "Psicoanalitica", "1 Hora", "Presenta estres academico", 3);
+        paciente3.agregarSesion(sesion31);
+        
+        tablaHashPacientes.put(paciente1.getRut(),paciente1);
+        tablaHashPacientes.put(paciente2.getRut(),paciente2);
+        tablaHashPacientes.put(paciente3.getRut(),paciente3);
+    }
 }
+
