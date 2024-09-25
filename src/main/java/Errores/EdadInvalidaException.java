7ad729374ea2c744fc29c9a4553fbd9b9f5efb26
Ian Guerrero <ian.guerrero.v@mail.pucv.cl>
2024-09-25 14:43:18 -0300

    UML y errores

---
+++ src/main/java/Errores/EdadInvalidaException.java
@@ -0,0 +1,8 @@
+
+package Errores;
+
+public class EdadInvalidaException extends Exception{
+    public EdadInvalidaException() {
+        super(" Edad Ingresada es invalida. ");
+    }
+}
--- src/main/java/Errores/RutInvalidoException.java
+++ src/main/java/Errores/RutInvalidoException.java
@@ -1,6 +1,8 @@
 
 package Errores;
 
-public class RutInvalidoException {
-    
+public class RutInvalidoException extends Exception{
+    public RutInvalidoException() {
+        super(" RUT ingresado es invalido. ");
+    }
 }
