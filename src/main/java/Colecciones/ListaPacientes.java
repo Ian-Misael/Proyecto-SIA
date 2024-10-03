fb1ced917ca6411be85cc117656b83f0fc2a5f78
Felipe <>
2024-10-03 00:29:18 -0300

    Metodos necesarios para el guardado de pacientes y sesiones al final

--- src/main/java/Colecciones/ListaPacientes.java
+++ src/main/java/Colecciones/ListaPacientes.java
@@ -2,7 +2,12 @@
 package Colecciones;
 
 import Clases.Paciente;
+import Clases.SesionTerapia;
 import Clases.Utilidades;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 
 public class ListaPacientes {
@@ -45,6 +50,59 @@ public class ListaPacientes {
             //cadena += paciente.listarSesiones();
         }
         return cadena;
+    }
+    
+    public void guardarPacientesCSV()
+    {
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("src/main/Datos/listadoPacientes.csv", false))) {
+
+            writer.write("NOMBRE;RUT;EDAD;DIRECCION;HISTORIAL MEDICO");
+            writer.newLine();
+            for (Paciente paciente : this.tablaHashPacientes.values()) {
+
+                writer.write(paciente.getNombre() + ";" + paciente.getRut() + ";" + paciente.getEdad() +
+                        ";" + paciente.getDireccion() + ";" + paciente.getHistorialMedico());
+                writer.newLine();
+            }
+
+        } catch (IOException e) {
+            System.out.println("Error al guardar los pacientes.");
+        }
+
+    }
+    public void guardarSesionesCSV()
+    {
+
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("src/main/Datos/listadoSesiones.csv", false))) {
+
+            writer.write("Rut ; Fecha ; Tipo de terapia ; Duracion ; Observaciones ; calificacion de mejora");
+            writer.newLine();
+            for (Paciente paciente : this.tablaHashPacientes.values()) {
+                ListaSesiones sesiones = paciente.ObtenerListaSesiones();
+                sesiones.reiniciarRecorrido();
+                for (int i = 0; i < sesiones.largo() ; i++)
+                {
+                    // Accedemos a la sesion
+                    sesiones.siguiente();
+                    SesionTerapia sesion = sesiones.obtenerSesionActual();
+
+                    // Con la sesion agregamos la linea en el csv
+                    DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+                    String fechaSTR = sesion.getFecha().format(formato);
+                    writer.write(paciente.getRut() + ";" + fechaSTR + ";" + sesion.getTipoTerapia() 
+                             + ";" + sesion.getDuracion() + ";" + sesion.getObservaciones() + ";" + sesion.getCalificacionMejora());
+                    writer.newLine();
+                }  
+            }
+
+
+
+        } catch (IOException e) {
+            System.out.println("Error al guardar las sesiones");
+        }
+
     }
 }
 
