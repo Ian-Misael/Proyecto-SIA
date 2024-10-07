333106ef096379335f8d4fc2d3691e234993e069
Felipe <felip@PC>
2024-10-06 23:43:20 -0300

    comentarios

--- src/main/java/Clases/Terapeuta.java
+++ src/main/java/Clases/Terapeuta.java
@@ -31,14 +31,18 @@ public class Terapeuta extends Persona {
     
     //Metodos de la clase
     
+    // Metodo para poder agregar sesiones a la lista del paciente.
     public void agregarSesion(SesionTerapia sesion) {
         sesiones.agregarSesion(sesion);
     }
     
+    // Metodo encargado de mostrar las sesiones de la lista.
     public String listarSesiones() {
         return sesiones.listarSesiones();
     }
     
+    // Sobreescritura de metodos
+    // Encargado de retornar los datos del terapeuta.
     @Override
     public String obtenerDatos() {
         return (getNombre() + "; " + getRut() + "; " + getEdad() + "; " + especialidad + "\n");
